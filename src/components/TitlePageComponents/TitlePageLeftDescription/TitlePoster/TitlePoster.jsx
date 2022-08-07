35b6209dc8f5ab3410e95ad982d854ee1cc08605
mista-a <dvaanddva@gmail.com>
2022-08-08 00:20:50 +0300

    change classes for common usage

--- src/components/TitlePageComponents/TitlePageLeftDescription/TitlePoster/TitlePoster.jsx
+++ src/components/TitlePageComponents/TitlePageLeftDescription/TitlePoster/TitlePoster.jsx
@@ -2,11 +2,11 @@ import { themoviedb } from '../../../../links'
 
 const TitlePoster = ({ posterPath }) => {
   return (
-    <div className='left-description__title-poster'>
+    <div className='left-description__poster-about'>
       <img
         src={`${themoviedb}/t/p/w600_and_h900_bestv2${posterPath}`}
         alt='title poster'
-        className='title-poster__image'
+        className='poster-about__image'
       />
     </div>
   )
